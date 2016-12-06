a1a17a0201b9877d9794cadb828e731f4a54ec6c
Adam Jolicoeur <adam.jolicoeur@gmail.com>
2016-12-06 12:34:54 -0500

    remove FA & update header
    remove Font Awesome as a son dependency due to CDN usage. update header
    information to be for my site.

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,8 @@ var pkg = require('./package.json');
 
 // Set the banner content
 var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * www.AdamJolicoeur.com - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2008-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
     ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
     ' */\n',
     ''
