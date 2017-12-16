6c057179acf3005ee1d15139dab9dbfb38c54085
Adam Jolicoeur <adam.jolicoeur@redhat.com>
2017-12-15 21:43:19 -0500

    upgrade to FA 5 Pro

--- gulpfile.js
+++ gulpfile.js
@@ -155,16 +155,6 @@ gulp.task('copy', function() {
 
   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
   .pipe(gulp.dest('vendor/jquery'))
-
-  gulp.src([
-    'node_modules/font-awesome/**',
-    '!node_modules/font-awesome/**/*.map',
-    '!node_modules/font-awesome/.npmignore',
-    '!node_modules/font-awesome/*.txt',
-    '!node_modules/font-awesome/*.md',
-    '!node_modules/font-awesome/*.json'
-  ])
-  .pipe(gulp.dest('vendor/font-awesome'))
 })
 
 // Configure the browserSync task
