8fbbce888b5f3d665018489179119abb1d2a52b2
Adam Jolicoeur <mindreeper2420@gmail.com>
2017-12-24 14:44:48 -0500

    version 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -150,11 +150,11 @@ gulp.task('minify-js', function() {
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-  gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-  .pipe(gulp.dest('vendor/bootstrap'))
+  // gulp.src(['node_modules/aos/dist/**'])
+  // .pipe(gulp.dest('vendor/aos'))
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-  .pipe(gulp.dest('vendor/jquery'))
+  // gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+  // .pipe(gulp.dest('vendor/jquery'))
 })
 
 // Configure the browserSync task
--- js/jolicoeur.js
+++ js/jolicoeur.js
@@ -1,15 +1,13 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
+AOS.init({
+  duration: 1200,
+});
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-custom").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-custom").removeClass("top-nav-collapse");
     }
 }
 
@@ -28,6 +26,6 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-custom ul li a').click(function() {
     $(this).closest('.collapse').collapse('toggle');
 });
