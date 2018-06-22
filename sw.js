e7ed203438d48dddb4e91ea5f5e82d3d495ff0ea
Adam Jolicoeur <adam.jolicoeur@redhat.com>
2018-06-22 11:22:01 -0400

    revert all data

---
+++ sw.js
@@ -0,0 +1,25 @@
+/*
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('airhorner').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/jolicoeur.min.css',
+        '/js/jolicoeur.min.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+*/
