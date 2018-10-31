62a4e18527b61afdc8a8f7444045baee6c71c8fe
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-10-31 11:01:06 -0400

    Move to Node 8 for Build System

--- css/jolicoeur.css
+++ css/jolicoeur.css
@@ -1,5 +1,5 @@
 /*!
- * Adam J. Jolicoeur - Adam J. Jolicoeur v2.0.0 (https://www.adamjolicoeur.com)
+ * Adam J. Jolicoeur - Adam J. Jolicoeur v2.16.0 (https://www.adamjolicoeur.com)
  * Copyright 2008-2018 Adam J. Jolicoeur
  * Licensed under Apache-2.0
  */
