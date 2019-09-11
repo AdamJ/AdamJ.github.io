ee1be939c51862715a2bcb844299b2629718c4b6
Adam Jolicoeur <mindreeper2420@gmail.com>
2019-09-10 20:22:37 -0400

    Update Gulp to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,138 +1,203 @@
-var gulp = require('gulp');
-var autoprefixer = require('autoprefixer');
-var browserSync = require('browser-sync').create();
-var clean = require('gulp-clean');
-var cssnano = require('cssnano');
-var header = require('gulp-header');
-var postcss = require('gulp-postcss');
-var pkg = require('./package.json');
-var pug = require('gulp-pug');
-var rename = require("gulp-rename");
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+(() => {
+  'use-strict';
+  const
 
-// Set the banner content
-var banner = ['/*!\n',
-' * <%= pkg.title %> <%=(pkg.homepage) %> | v<%= pkg.version %>\n',
-' * Copyright 2008-2019 | <%= pkg.title %>\n',
-' * Licensed under <%= pkg.license %> | (<%= pkg.licenseUrl %>)\n',
-' */\n',
-''
-].join('');
+    // development or production
+    devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() === 'development'),
 
-// compile custom scss files
-gulp.task('sass', function() {
-  return gulp.src("dev/sass/**/*.scss")
-  .pipe(sass().on('error', sass.logError))
-  .pipe(header(banner, { pkg: pkg }))
-  .pipe(gulp.dest('css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
-});
-
-// convert custom scss files to css using PostCSS
-// @ts-ignore
-gulp.task('css', ['sass'], function() {
-  var plugins = [
-      autoprefixer({browsers: ['last 1 version']}),
-      cssnano()
-  ];
-  return gulp.src('./css/jolicoeur.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss(plugins))
-    .pipe(sourcemaps.write())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./css'));
-});
-
-// copy FA5 webfonts
-gulp.task('webfonts', function() {
-  return gulp.src("node_modules/@fortawesome/fontawesome-free/webfonts/**")
-  .pipe(gulp.dest('fonts/webfonts'))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
-});
-
-// compile custom javascript file
-gulp.task('js', function() {
-  return gulp.src("dev/js/*.js")
-  .pipe(header(banner, { pkg: pkg }))
-  .pipe(gulp.dest('js'))
-  .pipe(browserSync.reload({
-    stream: true
-  }));
-});
-
-// compile pug templates
-gulp.task('views', function () {
-  return gulp.src('./src/*.pug')
-  .pipe(pug({
-    doctype: 'html',
-    pretty: true
-  }))
-  .pipe(gulp.dest('./'));
-});
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
+    // directory locations
+    dir = {
+      src    : './',
+      dev    : './dev/',
+      build  : './dist/'
     },
-    ui: {
-      port: 8001 // customize port for browserSync UI
-    },
-    port: 8080, // use 8080 to prevent conflicts with other localhosts
-    reloadOnRestart: true,
-    notify: false // prevent the browserSync notification from appearing
-  });
-});
 
-// ensure scss finishes, reload browser
-// @ts-ignore
-gulp.task('sass-watch', ['css'], function (done) {
-  browserSync.reload();
-  done();
-});
+    // modules
+    gulp              =  require('gulp'),
+    del               =  require('del'),
+    noop              =  require('gulp-noop'),
+    newer             =  require('gulp-newer'),
+    size              =  require('gulp-size'),
+    imagemin          =  require('gulp-imagemin'),
+    sass              =  require('gulp-sass'),
+    postcss           =  require('gulp-postcss'),
+    header            =  require('gulp-header'),
+    pug               =  require('gulp-pug'),
+    pkg               =  require('./package.json'),
+    rename            =  require('gulp-rename'),
+    clean             =  require('gulp-clean'),
+    sourcemaps        =  devBuild ? require('gulp-sourcemaps') : null,
+    browserSync       =  devBuild ? require('browser-sync') : null,
 
-gulp.task('copy-source', ['clean-dist'], function () {
-  gulp.src('./README.md').pipe(gulp.dest('./dist'));
-  gulp.src('./package.json').pipe(gulp.dest('./dist'));
-  gulp.src('./manifest.json').pipe(gulp.dest('./dist'));
-  gulp.src('./css/*.*').pipe(gulp.dest('./dist/css'));
-  gulp.src('./js/*.*').pipe(gulp.dest('./dist/js'));
-  gulp.src('./img/**/*.*').pipe(gulp.dest('./dist/img/'));
-  gulp.src('./fonts/**/*.*').pipe(gulp.dest('./dist/fonts/'));
-  gulp.src('./*.html').pipe(gulp.dest('./dist'));
-});
+    // Set the banner content
+    banner = ['/*!\n',
+    ' * <%= pkg.title %> <%=(pkg.homepage) %> | v<%= pkg.version %>\n',
+    ' * Copyright 2008-2019 | <%= pkg.title %>\n',
+    ' * Licensed under <%= pkg.license %> | (<%= pkg.licenseUrl %>)\n',
+    ' */\n',
+    ''
+    ].join('');
 
-gulp.task('build', ['copy-source']);
+    console.log('Gulp', devBuild ? 'development' : 'production', 'build');
 
-gulp.task('clean-dist', function () {
-  return gulp.src("dist/", {read: false})
-    .pipe(clean());
-});
+    /*
+    ** clean task
+    */
+    gulp.task('clean', (done) => {
+      del.sync([ dir.build ]);
 
-// Dev task with browserSync
-// @ts-ignore
-gulp.task('serve', ['css', 'js'], function () {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    },
-    ui: {
-      port: 8001 // customize port for browserSync UI
-    },
-    port: 8080, // use 8080 to prevent conflicts with other localhosts
-    reloadOnRestart: true,
-    notify: false // prevent the browserSync notification from appearing
-  });
-  gulp.watch('dev/sass/**/*.scss', ['sass-watch']);
-  gulp.watch('src/**/*.pug', ['views']);
-  gulp.watch('dev/js/*.js', ['js']);
-  gulp.watch('*.html').on('change', browserSync.reload);
-});
+      done();
+    });
 
-gulp.task('default', ['css', 'js', 'views']);
+    const cssConfig = {
+
+      dev         : dir.dev + 'sass/**/*.scss',
+      watch       : dir.src + 'sass/**/*.scss',
+      build       : dir.src + 'css',
+      release     : dir.build + 'css',
+
+      sassOpts: {
+        sourceMap       : devBuild,
+        outputStyle     : 'nested',
+        precision       : 3,
+        errLogToConsole : true
+      },
+
+      postCSS: [
+        require('autoprefixer')({
+          browsers: ['> 1%']
+        })
+      ]
+    };
+
+    if (!devBuild) {
+      cssConfig.postCSS.push(
+        require('usedcss')({ html: ['index.html'] }),
+        require('cssnano')
+      );
+    }
+
+    gulp.task('clean-dist', (done) => {
+      gulp.src("dist/", {read: false})
+      .pipe(clean());
+
+      done();
+    });
+
+    // convert custom scss files to css using PostCSS
+    // @ts-ignore
+    gulp.task('css', (done) => {
+      return gulp.src(cssConfig.dev)
+      .pipe(sourcemaps ? sourcemaps.init() : noop())
+      .pipe(sass(cssConfig.sassOpts).on('error', sass.logError))
+      .pipe(postcss(cssConfig.postCSS))
+      .pipe(sourcemaps ? sourcemaps.write() : noop())
+      .pipe(size({ showFiles:true }))
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulp.dest(cssConfig.build))
+      .pipe(gulp.dest(cssConfig.release))
+      .pipe(browserSync ? browserSync.reload({ stream: true }) : noop());
+    });
+
+    // compile custom javascript file
+    gulp.task('js', (done) => {
+      gulp.src("dev/js/*.js")
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulp.dest('js'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+      done();
+    });
+
+    // compile pug templates
+    gulp.task('views', (done) => {
+      gulp.src('./src/*.pug')
+      .pipe(pug({
+        doctype: 'html',
+        pretty: true
+      }))
+      .pipe(gulp.dest('./'));
+
+      done();
+    });
+
+    // Configure the browserSync task
+    gulp.task('browserSync', (done) => {
+      browserSync.init({
+        server: {
+          baseDir: ''
+        },
+        ui: {
+          port: 8001 // customize port for browserSync UI
+        },
+        port: 8080, // use 8080 to prevent conflicts with other localhosts
+        reloadOnRestart: true,
+        notify: false // prevent the browserSync notification from appearing
+      });
+
+      done();
+    });
+
+    // ensure scss finishes, reload browser
+    // @ts-ignore
+    gulp.task('sass-watch', gulp.series('css'), (done) => {
+      browserSync.reload();
+
+      done();
+    });
+
+    gulp.task('copy-source', gulp.series('clean-dist', (done) => {
+      gulp.src('./README.md').pipe(gulp.dest(dir.build));
+      gulp.src('./package.json').pipe(gulp.dest(dir.build));
+      gulp.src('./manifest.json').pipe(gulp.dest(dir.build));
+      gulp.src('./css/*.*').pipe(gulp.dest('./dist/css'));
+      gulp.src('./js/*.*').pipe(gulp.dest('./dist/js'));
+      gulp.src('./img/**/*.*').pipe(gulp.dest('./dist/img/'));
+      gulp.src('./fonts/**/*.*').pipe(gulp.dest('./dist/fonts/'));
+      gulp.src('./*.html').pipe(gulp.dest(dir.build));
+
+      done();
+    }));
+
+    gulp.task('build', gulp.series('copy-source', (done) => {
+      done();
+    }));
+
+    // Dev task with browserSync
+    // @ts-ignore
+    gulp.task('serve', gulp.series('css', 'js', (done) => {
+      browserSync.init({
+        server: {
+          baseDir: "./"
+        },
+        ui: {
+          port: 8001 // customize port for browserSync UI
+        },
+        port: 8080, // use 8080 to prevent conflicts with other localhosts
+        reloadOnRestart: true,
+        notify: false // prevent the browserSync notification from appearing
+      });
+      gulp.watch('dev/sass/**/*.scss', gulp.series('sass-watch'));
+      gulp.watch('src/**/*.pug', gulp.series('views'));
+      gulp.watch('dev/js/*.js', gulp.series('js'));
+      gulp.watch('*.html').on('change', browserSync.reload);
+
+      done();
+    }));
+
+    gulp.task('default', gulp.series('css', 'js', 'views', (done) => {
+      done();
+    }));
+
+    // copy FA5 webfonts
+    gulp.task('webfonts', function() {
+      gulp.src("node_modules/@fortawesome/fontawesome-free/webfonts/**")
+      .pipe(gulp.dest('fonts/webfonts'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
+    });
+
+})();
+
--- js/jolicoeur.js
+++ js/jolicoeur.js
@@ -1,5 +1,5 @@
 /*!
- * Adam J. Jolicoeur https://www.adamjolicoeur.com | v3.0.0
+ * Adam J. Jolicoeur https://www.adamjolicoeur.com | v0.0.0
  * Copyright 2008-2019 | Adam J. Jolicoeur
  * Licensed under Apache-2.0 | (https://github.com/mindreeper2420/mindreeper2420.github.io/blob/master/LICENSE)
  */
