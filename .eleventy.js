c498a679d30c2169484fb6e8f5d7979e05ddfe7b
Adam Jolicoeur <mindreeper2420@gmail.com>
2023-08-28 16:54:42 -0400

    Update Eleventy build config (#243)
    - Update Eleventy config file with "cleaner" file paths (remove the ./ from before src) and remove old lines.
    - Ran eleventy locally to test and found that new images should now be copied over to the gh-pages branch.

--- .eleventy.js
+++ .eleventy.js
@@ -7,25 +7,23 @@ const markdownItAnchor = require("markdown-it-anchor");
 const packageVersion = require("./package.json").version;
 const fs = require("fs");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-// const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addWatchTarget("src/sass/");
 
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("./src/keybase.txt");
-  eleventyConfig.addPassthroughCopy("./src/fonts");
-  eleventyConfig.addPassthroughCopy("./src/webfonts");
-  eleventyConfig.addPassthroughCopy("./src/img");
-  eleventyConfig.addPassthroughCopy("./src/favicon.*");
-  eleventyConfig.addPassthroughCopy("./src/cache-polyfill.js");
-  eleventyConfig.addPassthroughCopy("./src/sw.js");
-  eleventyConfig.addPassthroughCopy("./src/faviconData.json");
-  eleventyConfig.addPassthroughCopy("./src/manifest.json");
+  eleventyConfig.addPassthroughCopy("src/keybase.txt");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("src/webfonts");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/favicon.*");
+  eleventyConfig.addPassthroughCopy("src/cache-polyfill.js");
+  eleventyConfig.addPassthroughCopy("src/sw.js");
+  eleventyConfig.addPassthroughCopy("src/faviconData.json");
+  eleventyConfig.addPassthroughCopy("src/manifest.json");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addShortcode("packageVersion", () => `v${packageVersion}`);
