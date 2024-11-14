32b83a0c8ed4bdecc3c355bcf5cd821e2893e07c
AdamJ <AdamJ@users.noreply.github.com>
2024-11-14 14:48:23 +0000

    Shoelace Carousel, Buttons, Links
    Additional Shoelace updates
    
    Add Card Image Shoelace component
    
    Remove Bootstrap colors and components - Update ALM
    
    Update styles and CEA page
    
    Upgrade navigation, add new logo
    
    Fix responsive nav, update Testimonials
    
    Switch font families
    
    Update a11y for menu button
    
    Update a11y checks
    
    Update mobile navigation and page overflows
    
    Resolve layout issues
    
    Replace Nav element with class nav
    
    Add aria-label to menu checkbox 84d2ddfbdf01a69949105eabdc945f9784212da9

---
+++ js/jolicoeur.js
@@ -0,0 +1,15 @@
+/*!
+ * Adam J. Jolicoeur https://www.adamjolicoeur.com | v0.0.0
+ * Copyright 2008-2024 | Adam J. Jolicoeur
+ * Licensed under Apache-2.0 | (https://github.com/adamj/adamj.github.io/blob/main/LICENSE)
+ */
+
+// Shoelace Dialog Header
+// const dialog = document.querySelector('.dialog-header-actions');
+// const openButton = dialog.nextElementSibling;
+// const closeButton = dialog.querySelector('sl-button[slot="footer"]');
+// const newWindowButton = dialog.querySelector('.new-window');
+
+// openButton.addEventListener('click', () => dialog.show());
+// closeButton.addEventListener('click', () => dialog.hide());
+// newWindowButton.addEventListener('click', () => window.open(location.href));
