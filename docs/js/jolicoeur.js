31e9c6d39d82bb3c33f708628aa33e4198657915
AdamJ <adam.jolicoeur@gmail.com>
2025-09-08 10:48:08 -0400

    Delete Docs directory and package lock file

--- docs/js/jolicoeur.js
+++
@@ -1,5 +0,0 @@
-/*!
- * Adam J. Jolicoeur https://www.adamjolicoeur.com | v0.0.0
- * Copyright 2008-2024 | Adam J. Jolicoeur
- * Licensed under Apache-2.0 | (https://github.com/adamj/adamj.github.io/blob/main/LICENSE)
- */
